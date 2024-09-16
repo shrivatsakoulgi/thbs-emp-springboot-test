9365990decd9e0894b586f78a7c248ef727baf75
Shrivatsa Koulgi <89825785+shrivatsakoulgi@users.noreply.github.com>
2024-09-16 22:55:09 +0530

    Add files via upload

---
+++ test/AClassWithOneJUnitTest.java
@@ -0,0 +1,12 @@
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class AClassWithOneJUnitTest {
+
+    @Test
+    void demoTestMethod() {
+        assertTrue(true);
+    }
+}
